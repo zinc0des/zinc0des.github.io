7f644b2f1dde4b657519a0880054420b420ad3e7
Satavisha Roy <satavisha.roy@gmail.com>
2018-07-14 13:51:08 -0400

    odal not working

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showModal() {
     const modal = document.getElementById('modal');
     modal.classList.add('show');
 
-    modal.onclick = closeModal;
+    modal.onclick = closedModal;
 
     const modalContent = document.querySelector('#modal > div');
     modalContent.onclick = function(event) {
@@ -17,9 +17,25 @@ function showModal() {
     };
 }
 
-function closeModal() {
+function closedModal() {
     const modal = document.getElementById('modal');
     modal.classList.remove('show');
 }
 
 clickMe.onclick = showModal;
+
+const openModalBtn = document.getElementById('open-modal');
+const closeModalBtn = document.getElementById('close-modal');
+
+const openModal = function() {
+    document.main.classList.add('modal-open');
+};
+const closeModal = function() {
+    document.body.classList.remove('modal-open');
+  };
+  
+  openModalBtn.addEventListener('click', openModal);
+  closeModalBtn.addEventListener('click', closeModal);
+  
+ 
+
